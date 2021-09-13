279da2549b6ac680b948a3551311483e910c8ba0
DavidPerry06 <dperry@bayviewglen.ca>
2021-09-13 13:52:09 -0400

    escapes

--- Unit One - introduction to computer science/src/week1/ExampleThree.java
+++ Unit One - introduction to computer science/src/week1/ExampleThree.java
@@ -1,13 +1,12 @@
-/**
- * 
- */
+package week1;
+
 public class ExampleThree {
     public static void main(String[] args) {
       //  System.out.println("This is "very" important!"); //double quotes encapsulate string literals - putting a quote in a string literal gets java
     
       System.out.println("This is \"very\" important!"); // the backslash \ escapes a character - creates an escape sequence
     
-      System.out.println("This is \very important!"); // \v is not a valid escape sequence
+      //System.out.println("This is \very important!"); // \v is not a valid escape sequence
     
       System.out.println("This is \\very important!"); // you escape a \ with another \
       System.out.println("This\nis\nvery\nimportant!"); // \n is new line
