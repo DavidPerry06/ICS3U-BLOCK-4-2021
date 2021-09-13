c9f122c13258e2d9083ed2ec1a02ab2544901c93
DavidPerry06 <dperry@bayviewglen.ca>
2021-09-13 14:29:27 -0400

    Final modifier and double vs int

---
+++ Unit One - introduction to computer science/src/week2/ExampleSeven.java
@@ -0,0 +1,20 @@
+package week2;
+
+public class ExampleSeven {
+
+public static void main(String[] args) {
+    final int x = 7;
+    // x = 6;    // cannt assign a vlue to a final variable
+
+    final int y;
+
+    y = 8;
+    // y++; // cannot modify the variable once it is set if it is constant
+
+    final int NUM_STUDENTS = 18;
+
+
+    }
+
+}
+    
