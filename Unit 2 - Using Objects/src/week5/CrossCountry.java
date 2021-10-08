3c04156ba32e74349f94c35a39f245790fb43e0f
DavidPerry06 <dperry@bayviewglen.ca>
2021-10-08 14:31:43 -0400

    Cross Country Project

--- Unit 2 - Using Objects/src/week5/CrossCountry.java
+++ Unit 2 - Using Objects/src/week5/CrossCountry.java
@@ -3,6 +3,12 @@ package week5;
 import java.util.Scanner;
 
 public class CrossCountry {
+
+
+    /**
+     * initiates and closes the scanner, as well as calls the processRunner function
+     */
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
@@ -13,6 +19,13 @@ public class CrossCountry {
         in.close();
     }
 
+    /**
+     * processRunner function initiates the variables and gets String inputs and assigned them to the corresponding variable
+     * It then returns variables to the function substract time for the splits to be calculated
+     * After that it then prints out a summary of all the information into the console
+     * 
+     * @param in
+    */
     private static void processRunner(Scanner in) {
         String firstName, lastName;
         String mileOne, mileTwo, finish;
@@ -22,11 +35,11 @@ public class CrossCountry {
         firstName = in.nextLine();
         System.out.print("Please input your last name: ");
         lastName = in.nextLine();
-        System.out.print("Please input your first mile time: ");
+        System.out.print("Please input your first mile time(mm:ss.ddd): ");
         mileOne = in.nextLine();
-        System.out.print("Please input your second mile time: ");
+        System.out.print("Please input your second mile time(mm:ss.ddd): ");
         mileTwo = in.nextLine();
-        System.out.print("Please input your finish time: ");
+        System.out.print("Please input your finish time(mm:ss.ddd): ");
         finish = in.nextLine();
        
         splitTwo = substractTime(mileTwo, mileOne);
@@ -41,6 +54,12 @@ public class CrossCountry {
         
     }
 
+    /**
+     * This function initializes variables for the calculation of the times
+     * It does this by sending information to the function convertToSeconds to do the first part of changing the string to a double back into a string
+     * then it returns the double to the function converToTime to turn the int back into a string
+     */
+
     private static String substractTime(String endTime, String startTime) {
         double endTimeInSeconds = convertToSeconds(endTime);
         double startTimeInSeconds = convertToSeconds(startTime);
@@ -49,13 +68,26 @@ public class CrossCountry {
         
         return convertToTime(diffInSeconds);
     }
-
+    /**
+     * Turns the seconds into a (mm:ss.ddd) formulated string
+     * it does this by modulusing the time to get the seconds and dividing it to get the minutes
+     * It then returns the minutes and seconds into a (minutes:seconds.decimalseconds) style string to the main method
+     * 
+     */
     private static String convertToTime(double timeInSeconds) {
         int minutes = (int)(timeInSeconds/60);
         double seconds = timeInSeconds%60;
         
         return String.format("%d:%06.3f", minutes, seconds);
     }
+
+    /**
+     * Takes the string apart by seperating the front part of the string (the minutes) and the back end of the string (seconds and decimals) from the colon
+     * It Converts the front and back of the string to doubles using the Parse method
+     * It then multiplies the front end (the minutes) by 60 so that we can get the total seconds
+     * We then can add the front and back that have been converted to seconds to get the total seconds
+     * It then returns the sum in seconds of the front and back
+     */
 
     private static double convertToSeconds(String time) {
         int index = time.indexOf(":");
